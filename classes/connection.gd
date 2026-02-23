9ffa799455bffcf9ed50e5d4dd00755123cde533
CoolStopCode <coolstopmail@gmail.com>
2026-02-22 21:44:13 -0600

    rework connections 4
    maybe everything works???

--- classes/connection.gd
+++ classes/connection.gd
@@ -31,8 +31,36 @@ func _init(_from: Node2D, _from_port : int, _to: Node2D, _to_port : int, _line: 
 		to.ports_modified.connect(update)
 
 func update():
-	pass
-
+	if from.ports_out.size() <= from_port or to.ports_in.size() <= to_port:
+		free_connection()
+		return
+	
+	if from.ports_out[from_port].axis != to.ports_in[to_port].axis:
+		line.set_point_position(0, from.ports_out[from_port].position + from.global_position)
+		line.set_point_position(line.get_point_count() - 1, to.ports_in[to_port].position + to.global_position)
+		
+		if from.ports_out[from_port].axis == Constants.Axis.HORIZONTAL:
+			line.set_point_position(1, Vector2(line.get_point_position(1).x, from.ports_out[from_port].position.y + from.global_position.y))
+			line.set_point_position(line.get_point_count() - 2, Vector2(to.ports_in[to_port].position.x + to.global_position.x, line.get_point_position(line.get_point_count() - 2).y))
+		else:
+			line.set_point_position(1, Vector2(from.ports_out[from_port].position.x + from.global_position.x, line.get_point_position(1).y))
+			line.set_point_position(line.get_point_count() - 2, Vector2(line.get_point_position(line.get_point_count() - 2).x, to.ports_in[to_port].position.y + to.global_position.y))
+		return
+	line.set_point_position(0, from.ports_out[from_port].position + from.global_position)
+	if from.ports_out[from_port].axis == Constants.Axis.HORIZONTAL:
+		line.set_point_position(1, Vector2(line.get_point_position(1).x, from.ports_out[from_port].position.y + from.global_position.y))
+	else:
+		line.set_point_position(1, Vector2(from.ports_out[from_port].position.x + from.global_position.x, line.get_point_position(1).y))
+	
+	line.set_point_position(line.get_point_count() - 1, to.ports_in[to_port].position + to.global_position)
+	if from.ports_out[from_port].axis == Constants.Axis.HORIZONTAL:
+		line.set_point_position(line.get_point_count() - 2, 
+			Vector2(line.get_point_position(line.get_point_count() - 2).x, to.ports_in[to_port].position.y + to.global_position.y)
+		)
+	else:
+		line.set_point_position(line.get_point_count() - 2, 
+			Vector2(to.ports_in[to_port].position.x + to.global_position.x, line.get_point_position(line.get_point_count() - 2).y)
+		)
 
 func _on_endpoint_gone():
 	free_connection()
