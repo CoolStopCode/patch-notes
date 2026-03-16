d4742a53ee21736fd7f149aa881f71e6cb2d7660
CoolStopCode <coolstopmail@gmail.com>
2026-03-16 18:52:44 -0500

    start on note node

---
+++ nodes/patch_notes/output/note/note.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+signal actuate_output(port : int)
+
+@export var base_node : Node
+var properties : Array[InspectorProperty]
+
+var calculated_pitch : float
+
+func _ready() -> void:
+	properties = base_node.properties
+
+func property_changed(_property : InspectorProperty):
+	pass
+
+func emit_output(port := 0):
+	actuate_output.emit(port)
+
+func receive_input(_port := 0):
+	emit_output(0)
