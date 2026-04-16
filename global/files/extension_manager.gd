dbd28d04c9f72d4922f19b6cbbd3d583b5e36a78
CoolStopCode <coolstopmail@gmail.com>
2026-04-15 19:46:46 -0500

    embedded extensions

--- global/files/extension_manager.gd
+++ global/files/extension_manager.gd
@@ -1,1 +1,34 @@
 extends Node
+
+const SITE : String = "https://coolstopcode.github.io/PatchNodeExtensions/"
+
+func download_extension(git_path : String, as_name : String):
+	var request := HTTPRequest.new()
+	add_child(request)
+	request.request_completed.connect(_on_download_completed.bind(as_name))
+	request.request(git_path)
+
+func _on_download_completed(result, response_code, headers, body, as_name : String):
+	if response_code != 200:
+		print("Failed to download extension")
+		return
+	
+	var zip_path = FileManager.EXTENSIONS_PATH.path_join(as_name + ".zip")
+	var file = FileAccess.open(zip_path, FileAccess.WRITE)
+	file.store_buffer(body)
+	file.close()
+	var extracted_root := FileManager.extract_zip(zip_path, FileManager.EXTENSIONS_PATH)
+	DirAccess.remove_absolute(zip_path)
+	
+	var list_path = FileManager.EXTENSIONS_PATH.path_join(extracted_root).path_join("list.tres")
+	
+	FileManager.EXTENSION_LIST.extension_list.append(
+		load(list_path)
+	)
+
+func install_embedded_extensions():
+	FileManager.copy_dir_recursive("res://nodes/patch_notes", FileManager.EXTENSIONS_PATH)
+	DirAccess.copy_absolute(
+		"res://nodes".path_join("extension_list.tres"),
+		FileManager.EXTENSIONS_PATH.path_join("extension_list.tres")
+	)
--- global/files/file_manager.gd
+++ global/files/file_manager.gd
@@ -10,46 +10,80 @@ var PROJECTS_PATH = BASE_PATH + "/projects"
 
 var EXTENSION_LIST : ExtensionList
 
+func save_extension_list():
+	ResourceSaver.save(EXTENSION_LIST, EXTENSIONS_PATH.path_join("extension_list.tres"))
+
+func extract_zip(zip_path : String, dest_folder : String) -> String:
+	var reader = ZIPReader.new()
+	reader.open(zip_path)
+
+	# Destination directory for the extracted files (this folder must exist before extraction).
+	# Not all ZIP archives put everything in a single root folder,
+	# which means several files/folders may be created in `root_dir` after extraction.
+	var root_dir = DirAccess.open(dest_folder)
+
+	var top_level_folders := {}
+	var files = reader.get_files()
+	for file_path in files:
+		if file_path.begins_with("__MACOSX/"): continue
+		var parts = file_path.split("/")
+		if parts.size() > 0 and parts[0] != "":
+			top_level_folders[parts[0]] = true # takes the root of each folder in the zip
+		
+		# If the current entry is a directory.
+		if file_path.ends_with("/"):
+			root_dir.make_dir_recursive(file_path)
+			continue
+
+		# Write file contents, creating folders automatically when needed.
+		# Not all ZIP archives are strictly ordered, so we need to do this in case
+		# the file entry comes before the folder entry.
+		root_dir.make_dir_recursive(root_dir.get_current_dir().path_join(file_path).get_base_dir())
+		var file = FileAccess.open(root_dir.get_current_dir().path_join(file_path), FileAccess.WRITE)
+		var buffer = reader.read_file(file_path)
+		file.store_buffer(buffer)
+	
+	if top_level_folders.size() == 1: # if they all the same, then its the top
+		return top_level_folders.keys()[0]
+	else:
+		print("Messy zip file, no root")
+		print(top_level_folders)
+		return ""
+
+func copy_dir_recursive(from_path: String, to_path: String) -> void:
+	var dir = DirAccess.open(from_path)
+
+	# Create target directory if it doesn't exist
+	if not DirAccess.dir_exists_absolute(to_path):
+		DirAccess.make_dir_recursive_absolute(to_path)
+	
+	dir.list_dir_begin()
+	var file_name = dir.get_next()
+	
+	while file_name != "":
+		if dir.current_is_dir():
+			# Recursively copy subdirectories
+			copy_dir_recursive(from_path + "/" + file_name, to_path + "/" + file_name)
+		else:
+			# Copy individual files
+			dir.copy(from_path + "/" + file_name, to_path + "/" + file_name)
+		
+		file_name = dir.get_next()
+
+# Example Usage:
+# copy_dir("res://source_folder", "user://destination_folder")
+
+
 func _ready() -> void:
 	create_dir_if_not_exist(SOUNDS_PATH)
 	create_dir_if_not_exist(PROJECTS_PATH)
 	
 	if not DirAccess.dir_exists_absolute(EXTENSIONS_PATH):
 		DirAccess.make_dir_recursive_absolute(EXTENSIONS_PATH)
-		initial_extension_setup()
-	
-	EXTENSION_LIST = load(EXTENSIONS_PATH + "/extension_list.tres")
+		ExtensionManager.install_embedded_extensions()
+	FileManager.EXTENSION_LIST = load(FileManager.EXTENSIONS_PATH.path_join("extension_list.tres"))
+	FileManager.save_extension_list()
 
 func create_dir_if_not_exist(path : String):
 	if not DirAccess.dir_exists_absolute(path):
 		DirAccess.make_dir_recursive_absolute(path)
-
-func download_extension(git_path : String, as_name : String):
-	var request := HTTPRequest.new()
-	add_child(request)
-	request.request_completed.connect(_on_download_completed.bind(as_name))
-	request.request(git_path)
-
-func _on_download_completed(result, response_code, headers, body, as_name : String):
-	if response_code != 200:
-		print("Failed to download extension")
-		return
-	
-	var zip_path = EXTENSIONS_PATH + ("/%s.zip" % as_name)
-	var file = FileAccess.open(zip_path, FileAccess.WRITE)
-	file.store_buffer(body)
-	file.close()
-	Constants.extract_zip(zip_path, EXTENSIONS_PATH)
-	print("Extension extracted")
-	
-	EXTENSION_LIST.extension_list.append(
-		load(EXTENSIONS_PATH + "/%s/list.tres" % as_name)
-	)
-	ResourceSaver.save(EXTENSION_LIST, EXTENSIONS_PATH + "/extension_list.tres")
-
-func initial_extension_setup():
-	var extension_list := ExtensionList.new()
-	EXTENSION_LIST = extension_list
-	ResourceSaver.save(EXTENSION_LIST, EXTENSIONS_PATH + "/extension_list.tres")
-	
-	download_extension(SITE + "extensions/control/control.zip", "control")
