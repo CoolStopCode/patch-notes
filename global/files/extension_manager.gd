bc077a5cbf12f84707dfffb1f007d9505875afcf
CoolStopCode <coolstopmail@gmail.com>
2026-04-18 12:03:43 -0500

    trying out pck extensions

--- global/files/extension_manager.gd
+++ global/files/extension_manager.gd
@@ -13,41 +13,60 @@ func _on_download_completed(result, response_code, headers, body, as_name : Stri
 		print("Failed to download extension")
 		return
 	
-	var zip_path = FileManager.EXTENSIONS_PATH.path_join(as_name + ".zip")
-	var file = FileAccess.open(zip_path, FileAccess.WRITE)
+	# Write the downloaded bytes to a .pck file
+	var file = FileAccess.open(FileManager.EXTENSIONS_PATH.path_join(as_name + ".pck"), FileAccess.WRITE)
+	if not file:
+		push_error("Failed to write extension: " + as_name)
+		return
 	file.store_buffer(body)
 	file.close()
-	var extracted_root := FileManager.extract_zip(zip_path, FileManager.EXTENSIONS_PATH)
-	DirAccess.remove_absolute(zip_path)
 	
-	var list_path = FileManager.EXTENSIONS_PATH.path_join(extracted_root).path_join("list.tres")
-	var list = load(list_path)
+	# Mount it immediately without needing a restart
 	
-	ConfigManager.add_default_pins(list)
-	FileManager.EXTENSION_LIST.extension_list.append(
-		list
-	)
+	
+	var extension = load("res://" + as_name + "/list.tres")
+	if not extension:
+		push_error("No list.tres found in extension: " + as_name)
+		return
+	
+	FileManager.EXTENSION_LIST.extension_list.append(extension)
+	FileManager.save_extension_list()
+	
+	print("Extension installed and mounted: " + as_name)
+	
+	#var zip_path = FileManager.EXTENSIONS_PATH.path_join(as_name + ".zip")
+	#var file = FileAccess.open(zip_path, FileAccess.WRITE)
+	#file.store_buffer(body)
+	#file.close()
+	#var extracted_root := FileManager.extract_zip(zip_path, FileManager.EXTENSIONS_PATH)
+	#DirAccess.remove_absolute(zip_path)
+	#
+	#var list_path = FileManager.EXTENSIONS_PATH.path_join(extracted_root).path_join("list.tres")
+	#var list = load(list_path)
+	#
+	#ConfigManager.add_default_pins(list)
+	#FileManager.EXTENSION_LIST.extension_list.append(
+		#list
+	#)
+
+func mount_extensions():
+	for extension in FileManager.EXTENSION_LIST.extension_list:
+		var success = ProjectSettings.load_resource_pack(FileManager.EXTENSIONS_PATH.path_join(as_name + ".pck"))
+		if not success:
+			push_error("Failed to mount extension: " + as_name)
+			return
 
 func install_embedded_extensions():
-	FileManager.copy_dir_recursive("res://nodes/patch_notes", FileManager.EXTENSIONS_PATH)
-	DirAccess.copy_absolute(
-		"res://nodes".path_join("extension_list.tres"),
-		FileManager.EXTENSIONS_PATH.path_join("extension_list.tres")
-	)
-	var list : ExtensionList = load(FileManager.EXTENSIONS_PATH.path_join("extension_list.tres"))
-	for extension in list.extension_list:
-		ConfigManager.add_default_pins(extension)
-
-func rebuild_references_of_extension(extension : Extension):
-	# loop through the extensions node_list
-	for node_type in extension.node_list:
-		pass
-
-func rebuild_reference():
-	# get resource_path of the resource
-	# convert res:// path to global filesystem path
-	# go to that location and set the resource there to a variable
-	# go back to the reference and set it to that variable
+	#FileManager.copy_dir_recursive("res://nodes/patch_notes", FileManager.EXTENSIONS_PATH)
+	#DirAccess.copy_absolute(
+		#"res://nodes".path_join("extension_list.tres"),
+		#FileManager.EXTENSIONS_PATH.path_join("extension_list.tres")
+	#)
+	#var list : ExtensionList = load(FileManager.EXTENSIONS_PATH.path_join("extension_list.tres"))
+	#for extension in list.extension_list:
+		#ConfigManager.add_default_pins(extension)
+	var extension_list := ExtensionList.new()
+	FileManager.EXTENSION_LIST = extension_list
+	ResourceSaver.save(FileManager.EXTENSION_LIST, FileManager.EXTENSIONS_PATH + "/extension_list.tres")
 	
-	
-	pass
+	download_extension(SITE + "extensions/control.pck", "control")
